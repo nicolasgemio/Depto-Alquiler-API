ad92fc6f016c82fbb35ad3da9df7f600c7fe294a
nicolas gemio <nicolasgemio@outlook.com>
2025-08-03 20:44:21 -0300

    actualización

---
+++ config/oauth.py
@@ -0,0 +1,16 @@
+from authlib.integrations.starlette_client import OAuth
+from dotenv import load_dotenv
+import os
+
+load_dotenv('config/.env')
+
+oauth = OAuth()
+oauth.register(
+    name='google',
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
